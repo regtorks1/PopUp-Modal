d372b2445b046a2e8079bc62cc3444e89f5349bb
regtorks1 <reginald.torkonoo@amalitech.org>
2021-06-25 12:50:41 +0100

    Pop-Modal Code for reuse

---
+++ pop-modal.js
@@ -0,0 +1,8 @@
+const showModal = () => {
+    document.getElementById("toggle_modal").style.visibility = "visible";
+}
+
+// close Modal
+const closeModal = () => {
+    document.getElementById("toggle_modal").style.visibility = "hidden";
+}
